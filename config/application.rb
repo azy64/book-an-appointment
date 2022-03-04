3ee509dc9cb80dde4f73f7853f033026c8cc766b
azy64 <allysaidi64@gmail.com>
2022-03-04 13:08:16 +0100

    Fix the flash error

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,8 @@ module BookAppointment
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-    config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore, :key => '_namespace_key')
+    config.middleware.use ActionDispatch::Flash
+    # config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore, :key => '_namespace_key')
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users,
+  devise_for :users, defaults: { format: :json },
              controllers: {
                  sessions: 'users/sessions',
                  registrations: 'users/registrations'
              }
   # get '/member-data', to: 'members#show'
 
-  resources :reservations
+  
   resources :addresses
   resources :doctors
-  resources :users
+  resources :users do
+    resources :reservations
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
