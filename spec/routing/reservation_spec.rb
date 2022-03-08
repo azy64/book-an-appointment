eaf825870bb7041daa328e780ba29a8de1c02199
victor-chukwu <okenwa1993@gmail.com>
2022-03-08 14:12:52 +0100

    Test reservation index and new

--- spec/routing/reservation_spec.rb
+++ spec/routing/reservation_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ReservationsController, type: :routing do
+    describe 'routing' do
+        it 'routes to #index' do
+            expect(get: '/users/1/reservations').to route_to('reservations#index', user_id: '1')
+        end
+         # it 'routes to #new' do
+        #   expect(get: '/doctors/new').to route_to('doctors#new')
+        # end
+    end
 end
