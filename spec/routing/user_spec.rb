6c8193a241df4d0c008b1eef2ce9480f97e7e629
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-03-09 07:49:51 +0200

    Write update via PUT, PATCH  and destroy routing user test

--- spec/routing/user_spec.rb
+++ spec/routing/user_spec.rb
@@ -10,12 +10,20 @@ RSpec.describe UsersController, type: :routing do
     it 'routes to #show' do
       expect(get: '/users/1').to route_to('users#show', id: '1')
     end
-    it 'routes to #edit' do
-      expect(get: '/users/1/edit').to route_to('users#edit', id: '1')
+    # it 'routes to #edit' do
+    #   expect(get: '/users/1/edit').to route_to('users#edit', id: '1')
+    # end
+    # it 'routes to #create' do
+    #   expect(post: '/users').to route_to('Users/registrations#create')
+    # end
+    it 'routes to #update via PUT' do
+      expect(put: '/users/1').to route_to('users#update', id: '1')
     end
-    it 'routes to #create' do
-      expect(post: '/users').to route_to('Users/registrations#create')
+    it 'routes to #update via PATCH' do
+      expect(patch: '/users/1').to route_to('users#update', id: '1')
     end
-    
+    it 'routes to #destroy' do
+      expect(delete: '/users/1').to route_to('users#destroy', id: '1')
+    end
   end
 end
