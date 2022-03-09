03fb8c3ad49a6f2ba6b481fdf1306b7303c6546c
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-03-09 07:48:23 +0200

    Write show edit and create routing user test

--- spec/routing/user_spec.rb
+++ spec/routing/user_spec.rb
@@ -4,8 +4,17 @@ RSpec.describe UsersController, type: :routing do
     it 'routes to #index' do
       expect(get: '/users').to route_to('users#index')
     end
-    it 'routes to #new' do
-      expect(get: '/users/new').to route_to('users#new')
+    # it 'routes to #new' do
+    #   expect(get: '/users/new').to route_to('users#new')
+    # end
+    it 'routes to #show' do
+      expect(get: '/users/1').to route_to('users#show', id: '1')
+    end
+    it 'routes to #edit' do
+      expect(get: '/users/1/edit').to route_to('users#edit', id: '1')
+    end
+    it 'routes to #create' do
+      expect(post: '/users').to route_to('Users/registrations#create')
     end
     
   end
